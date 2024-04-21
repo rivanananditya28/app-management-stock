c36c23af4bf6dbb22e2e9149fb4781103a610928
unknown <ananditya.pradana@gmail.com>
2024-04-21 19:38:09 +0700

    fix

--- maintenance_stok/stok.php
+++ maintenance_stok/stok.php
@@ -2,6 +2,71 @@
 include '../koneksi.php';
 date_default_timezone_set('Asia/Jakarta');
 
+function validateJenisTransaksi($str)
+{
+        // Validasi jenis transaksi
+        if ($str != "masuk" || $str != "keluar") {
+                return true;
+        }
+        // return true;
+}
+
+function validateBukti($str)
+{
+        if (strpos($str, ' ') !== false) {
+                return true;
+        }
+        if (bukti_sudah_ada($str)) {
+                return false;
+        } else {
+                return true;
+        }
+}
+
+function validateQuantity($str)
+{
+        $str = trim($str);
+        if (is_numeric($str) && $str > 0) {
+                return true;
+        } else {
+                return false;
+        }
+}
+
+// Fungsi untuk memeriksa apakah bukti sudah ada di dalam database
+function bukti_sudah_ada($bukti)
+{
+        // Lakukan koneksi ke database, sesuaikan dengan pengaturan database kamu
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "db_inventory";
+
+        // Buat koneksi
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        // Periksa koneksi
+        if ($conn->connect_error) {
+                die("Koneksi ke database gagal: " . $conn->connect_error);
+        }
+
+        // Lakukan kueri untuk memeriksa apakah bukti sudah ada
+        $sql = "SELECT COUNT(*) AS count FROM transaksi_history WHERE bukti = '$bukti'";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+                $row = $result->fetch_assoc();
+                $count = $row["count"];
+                // Jika count > 0, berarti bukti sudah ada di database
+                if ($count > 0) {
+                        $conn->close();
+                        return true;
+                }
+        }
+
+        $conn->close();
+        return false;
+}
 //supaya nama barang keluar sesuai kode barang yg dipilih
 if (isset($_POST['kode_barang'])) {
         $kode_barang = $_POST['kode_barang'];
@@ -22,8 +87,6 @@ if (isset($_POST['kode_barang'])) {
 
 // Proses transaksi
 if (isset($_POST['submit'])) {
-        // var_dump($_POST['jenis_transaksi'],$_POST['bukti'],$_POST['lokasi'],$_POST['kode_barang'],$_POST['nama_barang'],$_POST['tgl_transaksi'],$_POST['qty']);
-        // die();
         // Ambil data dari form
         $jenis_transaksi        = $_POST['jenis_transaksi'];
         $bukti                  = $_POST['bukti'];
@@ -32,25 +95,33 @@ if (isset($_POST['submit'])) {
         $nama_barang            = $_POST['nama_barang'];
         $date                   = date_create_from_format("d/m/Y", $_POST['tgl_transaksi']);
         $tgl_transaksi          = date_format($date, 'Y-m-d');
-        // $tgl_transaksi          = date("Y-m-d", strtotime($_POST['tgl_transaksi']));
-        // var_dump($tgl_transaksi);
-        // die();
         $qty                    = $_POST['qty'];
         $tanggal                = date("Y-m-d H:i:s");
-        // var_dump($tanggal);
-        // die();
-        // $jam                    = date("h:i:s");
-        $user                   = "user";
+        $user                   = $_POST['user'];
 
         // Lakukan validasi input kosong
-        if (empty($jenis_transaksi) || empty($bukti) || empty($lokasi) || empty($kode_barang) || empty($nama_barang) || empty($qty)) {
-                // var_dump(empty($nik),empty($tgl_masuk),empty($nama),empty($alamat),empty($kota),empty($gelar));
+        if (empty($jenis_transaksi) || empty($bukti) || empty($lokasi) || empty($kode_barang) || empty($nama_barang) || empty($qty) || empty($user)) {
                 $errors[] = "Semua kolom harus diisi.";
+        }
+
+        // Lakukan validasi
+        if (!validateJenisTransaksi($jenis_transaksi)) {
+                // var_dump($jenis_transaksi);
+                // die();
+                $errors[] = "Jenis Transaksi Tidak Sesuai";
+        }
+
+        if (!validateBukti($bukti)) {
+                $errors[] = "Bukti Sudah Ada";
+        }
+
+        if (!validateQuantity($qty)) {
+                $errors[] = "Qty harus berupa angka non negatif";
         }
 
         if ($jenis_transaksi == "masuk") {
                 // Ambil tgl masuk terakhir dari saldo barang pada lokasi dan kode barang yang bersangkutan
-                $query_last_entry_date = "SELECT MAX(tanggal_masuk) AS last_entry_date FROM stok_barang WHERE kode_barang = '$kode_barang' AND id_lokasi = '$lokasi'";
+                $query_last_entry_date = "SELECT MAX(tanggal_masuk) AS last_entry_date FROM stok_barang WHERE id_barang = '$kode_barang' AND id_lokasi = '$lokasi'";
                 $result_last_entry_date = $conn->query($query_last_entry_date);
                 $row_last_entry_date = $result_last_entry_date->fetch_assoc();
                 $last_entry_date = $row_last_entry_date['last_entry_date'];
@@ -60,7 +131,7 @@ if (isset($_POST['submit'])) {
                 $tgl_transaksi = date_format($date_trans, 'Y-m-d');
 
                 // Bandingkan tgl transaksi dengan tgl masuk terakhir
-                if ($tgl_transaksi <= $last_entry_date) {
+                if ($tgl_transaksi < $last_entry_date) {
                         $errors[] = "Tanggal transaksi harus setelah tanggal masuk terakhir.";
                 }
                 if (empty($tgl_transaksi)) {
@@ -70,7 +141,7 @@ if (isset($_POST['submit'])) {
 
         if ($jenis_transaksi == "keluar") {
                 // Ambil tgl masuk terakhir dari saldo barang pada lokasi dan kode barang yang bersangkutan
-                $query_last_entry_date = "SELECT MAX(tanggal_masuk) AS last_entry_date FROM stok_barang WHERE kode_barang = '$kode_barang' AND id_lokasi = '$lokasi'";
+                $query_last_entry_date = "SELECT MAX(tanggal_masuk) AS last_entry_date FROM stok_barang WHERE id_barang = '$kode_barang' AND id_lokasi = '$lokasi'";
                 $result_last_entry_date = $conn->query($query_last_entry_date);
                 $row_last_entry_date = $result_last_entry_date->fetch_assoc();
                 $last_entry_date = $row_last_entry_date['last_entry_date'];
@@ -83,8 +154,10 @@ if (isset($_POST['submit'])) {
                 if ($tgl_transaksi < $last_entry_date) {
                         $errors[] = "Tanggal transaksi harus setelah atau sama dengan tanggal masuk terakhir.";
                 }
+
                 // Ambil saldo barang pada lokasi dan kode barang yang bersangkutan
-                $query_saldo_barang = "SELECT saldo FROM stok_barang WHERE kode_barang = '$kode_barang' AND id_lokasi = '$lokasi'";
+                $query_saldo_barang = "SELECT sum(saldo) as saldo FROM stok_barang WHERE id_barang = '$kode_barang' AND id_lokasi = '$lokasi'";
+                // $query_saldo_barang = "SELECT saldo FROM stok_barang WHERE kode_barang = '$kode_barang' AND id_lokasi = '$lokasi'";
                 $result_saldo_barang = $conn->query($query_saldo_barang);
                 if ($result_saldo_barang->num_rows > 0) {
                         $row_saldo_barang = $result_saldo_barang->fetch_assoc();
@@ -112,35 +185,80 @@ if (isset($_POST['submit'])) {
         }
 
         if ($jenis_transaksi == "masuk") {
-                // Update stok barang
-                $query = "INSERT INTO stok_barang (kode_barang, id_lokasi, saldo, tanggal_masuk) 
-                        VALUES ('$kode_barang', '$lokasi', '$qty', '$tgl_transaksi')
-                        ON DUPLICATE KEY UPDATE saldo = saldo + $qty";
-                $conn->query($query);
+                // // Update stok barang
+                // $query = "INSERT INTO stok_barang (id_barang, id_lokasi, saldo, tanggal_masuk) 
+                //         VALUES ('$kode_barang', '$lokasi', '$qty', '$tgl_transaksi')
+                //         ON DUPLICATE KEY UPDATE saldo = saldo + $qty";
+                // $conn->query($query);
+
+                // // ambil id_stok
+                // $query_id_stok = "SELECT id FROM stok_barang WHERE id_barang = '$kode_barang' AND id_lokasi = '$lokasi' AND tanggal_masuk = '$tgl_transaksi'";
+                // $result_id_stok = $conn->query($query_id_stok);
+                // $row_id_stok = $result_id_stok->fetch_assoc();
+                // $query_id_stok = $row_id_stok['id'];
+
+                // // Masukkan transaksi ke database
+                // $sql = "INSERT INTO transaksi_history (bukti, tanggal_transaksi, qty, program, id_user, id_stok)
+                //         VALUES ('$bukti', '$tanggal', '$qty', '$jenis_transaksi', '$user', '$query_id_stok')";
+                // $conn->query($sql);
+
+                // Cek apakah tanggal masuk sudah ada dalam record tabel stok (revisi no.6)
+                $query_check = "SELECT id FROM stok_barang WHERE id_barang = '$kode_barang' AND id_lokasi = '$lokasi' AND tanggal_masuk = '$tgl_transaksi'";
+                $result_check = $conn->query($query_check);
+
+                if ($result_check->num_rows > 0) {
+                        // Jika tanggal masuk sudah ada, lakukan update jumlah stok
+                        $query_update = "UPDATE stok_barang SET saldo = saldo + $qty WHERE id_barang = '$kode_barang' AND id_lokasi = '$lokasi' AND tanggal_masuk = '$tgl_transaksi'";
+                        $conn->query($query_update);
+
+                        // Ambil id_stok
+                        $row_id_stok = $result_check->fetch_assoc();
+                        $query_id_stok = $row_id_stok['id'];
+                } else {
+                        // Jika tanggal masuk belum ada, lakukan insert baru
+                        $query_insert = "INSERT INTO stok_barang (id_barang, id_lokasi, saldo, tanggal_masuk) 
+                    VALUES ('$kode_barang', '$lokasi', '$qty', '$tgl_transaksi')";
+                        $conn->query($query_insert);
+
+                        // Ambil id_stok dari record yang baru saja di-insert
+                        $query_id_stok = $conn->insert_id;
+                }
 
                 // Masukkan transaksi ke database
-                $sql = "INSERT INTO transaksi_history (bukti, tanggal_transaksi, id_lokasi, kode_barang, qty, program, user)
-                        VALUES ('$bukti', '$tanggal', '$lokasi', '$kode_barang', '$qty', '$jenis_transaksi', '$user')";
+                $sql = "INSERT INTO transaksi_history (bukti, tanggal_transaksi, qty, program, id_user, id_stok)
+                        VALUES ('$bukti', '$tanggal', '$qty', '$jenis_transaksi', '$user', '$query_id_stok')";
                 $conn->query($sql);
         } else {
                 // Implementasi FIFO
-                $query = "SELECT id, saldo FROM stok_barang WHERE kode_barang = '$kode_barang' ORDER BY id ASC";
+                $query = "SELECT id, saldo FROM stok_barang WHERE id_barang = '$kode_barang' AND id_lokasi = '$lokasi' ORDER BY tanggal_masuk ASC";
                 $result = mysqli_query($conn, $query);
                 $total_qty = $qty;
-                // $row = mysqli_fetch_assoc($result);
-                // $saldo = $row['saldo'];
+                // $hasil ='';
                 while ($row = mysqli_fetch_assoc($result)) {
                         if ($row['saldo'] >= $total_qty) {
                                 $query = "UPDATE stok_barang SET saldo = saldo - $total_qty WHERE id = {$row['id']}";
                                 mysqli_query($conn, $query);
+                                // insert transaksi_history
+                                $sql = "INSERT INTO transaksi_history (bukti, tanggal_transaksi, qty, program, id_user, id_stok)
+                                        VALUES ('$bukti', '$tanggal', '$total_qty', '$jenis_transaksi', '$user', '{$row['id']}')";
+                                $conn->query($sql);
                                 break;
                         } else {
                                 $query = "UPDATE stok_barang SET saldo = 0 WHERE id = {$row['id']}";
                                 mysqli_query($conn, $query);
                                 $total_qty -= $row['saldo'];
+                                // insert transaksi_history
+                                $sql = "INSERT INTO transaksi_history (bukti, tanggal_transaksi, qty, program, id_user, id_stok)
+                                        VALUES ('$bukti', '$tanggal', '{$row['saldo']}', '$jenis_transaksi', '$user', '{$row['id']}')";
+                                $conn->query($sql);
                         }
                 }
         }
-        // Redirect ke halaman lain atau tampilkan pesan sukses
-        echo '<script> window.location.href = "index.php";</script>';
+        session_start();
+        // ketika di framework sudah ada fitur flash data message (session)
+        // alert hanya muncul sekali ketika di refresh halaman alertnya hilang, tetapi kalau di php native belum ada fitur tersebut (buat sendiri)
+        $_SESSION['message'] = '<div class="alert alert-success" role="alert">Berhasil disimpan!</div>';
+        header("Location: index.php");
+        exit();
+        // echo '<script> window.location.href = "index.php";</script>';
 }
--- report/transaksi_history/index.php
+++ report/transaksi_history/index.php
@@ -1,5 +1,8 @@
 <!DOCTYPE html>
 <html>
+<?php include '../../koneksi.php';
+// date_default_timezone_set('Asia/Jakarta');
+?>
 
 <head>
     <!-- DataTables Library -->
@@ -25,9 +28,235 @@
             </div>
         </div>
     </nav>
+    <div class="container">
+        <div class="card mt-5">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <p>Filter</p>
+                            </div>
+                            <div class="card-body">
+                                <form id="filterForm" method="get">
+                                    <div class="row">
+                                        <div class="col-3">
+                                            <div class="form-group mx-3">
+                                                <label>Bukti :</label>
+                                                <input type="text" id="bukti" name="bukti" class="form-control" onchange="change()" value="<?php echo isset($_GET['bukti']) ? $_GET['bukti'] : ''; ?>">
+                                            </div>
+                                        </div>
+                                        <div class="col-3">
+                                            <div class="form-group mx-3">
+                                                <label>Tanggal Transaksi :</label>
+                                                <input type="text" id="tgl_transaksi" name="tgl_transaksi" class="form-control datepicker" onchange="change()" value="<?php echo isset($_GET['tgl_transaksi']) ? $_GET['tgl_transaksi'] : ''; ?>" autocomplete="off">
+                                            </div>
+                                        </div>
+                                        <div class="col-3">
+                                            <div class="form-group mx-3">
+                                                <label>Lokasi :</label>
+                                                <input type="text" id="lokasi" name="lokasi" class="form-control" onchange="change()" value="<?php echo isset($_GET['lokasi']) ? $_GET['lokasi'] : ''; ?>">
+                                            </div>
+                                        </div>
+                                        <div class="col-3">
+                                            <div class="form-group mx-3">
+                                                <label>Kode Barang :</label>
+                                                <input type="text" id="kode_barang" name="kode_barang" class="form-control" onchange="change()" value="<?php echo isset($_GET['kode_barang']) ? $_GET['kode_barang'] : ''; ?>">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mt-3">
+                    <div class="col-12">
+                        <table id="table_id" class="datatable table table-striped">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Bukti</th>
+                                    <th>Tanggal Transaksi</th>
+                                    <th>Jam Transaksi</th>
+                                    <th>Lokasi</th>
+                                    <th>Kode Barang</th>
+                                    <th>Tgl Masuk</th>
+                                    <th>Qty</th>
+                                    <th>Program</th>
+                                    <th>User</th>
+                                    <!-- <th style="width: 168px;">
+                                        <center>Action</center>
+                                    </th> -->
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                //mengembalikan semua catatan dari tabel kiri (tabel1), dan catatan yang cocok dari tabel kanan 
+                                $query = "SELECT
+                                        * 
+                                    FROM
+                                        transaksi_history
+                                        INNER JOIN stok_barang ON transaksi_history.id_stok = stok_barang.id
+                                        INNER JOIN lokasi ON stok_barang.id_lokasi = lokasi.id
+                                        INNER JOIN barang ON stok_barang.id_barang = barang.id
+                                        INNER JOIN user ON transaksi_history.id_user = user.id";
+
+                                // // Ambil nilai parameter dari JavaScript
+                                $kode_barang = isset($_GET['kode_barang']) ? $_GET['kode_barang'] : '';
+                                $lokasi = isset($_GET['lokasi']) ? $_GET['lokasi'] : '';
+                                $bukti = isset($_GET['bukti']) ? $_GET['bukti'] : '';
+                                // $tgl_transaksi = isset($_GET['tgl_transaksi']) ? $_GET['tgl_transaksi'] :'';
+                                if (!empty($_GET["tgl_transaksi"])) {
+                                    $tgl_transaksi = DateTime::createFromFormat("d/m/Y", $_GET["tgl_transaksi"]);
+                                    if ($tgl_transaksi) {
+                                        $tgl_transaksi_formatted = $tgl_transaksi->format("Y-m-d"); // Konversi ke format yang sesuai dengan tipe data timestamp di database
+                                        $query .= " AND DATE(tanggal_transaksi) = '$tgl_transaksi_formatted'";
+                                    } else {
+                                        echo "Format Tanggal Salah";
+                                    }
+                                }
+
+                                if (!empty($kode_barang)) {
+                                    $query .= " AND barang.id_barang = '$kode_barang'";
+                                }
+                                if (!empty($lokasi)) {
+                                    $query .= " AND lokasi.id_lokasi = '$lokasi'";
+                                }
+                                // if (!empty($tgl_transaksi)) {
+                                //     $query .= " AND tanggal_transaksi = '$tgl_transaksi'";
+                                // }
+                                if (!empty($bukti)) {
+                                    $query .= " AND transaksi_history.bukti = '$bukti'";
+                                }
+
+                                $result = $conn->query($query);
+                                if ($result === false) {
+                                    die("Error dalam eksekusi query: " . $conn->error);
+                                }
+                                // var_dump($result->fetch_assoc());
+                                // die();
+                                $no = 1;
+                                while ($row = $result->fetch_assoc()) {
+                                ?>
+                                    <tr>
+                                        <td><?= $no++; ?></td>
+                                        <td><?= $row['bukti']; ?></td>
+                                        <td><?= date('d/m/Y', strtotime($row['tanggal_transaksi'])); ?></td>
+                                        <td><?= date('H:i:s', strtotime($row['tanggal_transaksi'])); ?></td>
+                                        <td><?= $row['kode_lokasi']; ?></td>
+                                        <td><?= $row['kode_barang']; ?></td>
+                                        <td><?= date('d/m/Y', strtotime($row['tanggal_masuk'])); ?></td>
+                                        <!-- <td><?= $row['tanggal_masuk']; ?></td> -->
+                                        <td>
+                                            <?php
+                                            if ($row['program'] == "keluar") {
+                                                echo "-";
+                                            }
+                                            ?>
+                                            <?= $row['qty']; ?>
+                                        </td>
+                                        <td><?= $row['program']; ?></td>
+                                        <td><?= $row['nama']; ?></td>
+                                        <!-- <td><?php
+                                                    if ($row['program'] == "masuk") {
+                                                        echo '<center>';
+                                                        echo '<a href="../../maintenance_stok/index.php?id=' . $row['id'] . '" class="btn btn-warning btn-sm">Edit</a>';
+                                                        echo '&nbsp;';
+                                                        echo '<button class="btn btn-danger btn-sm" onclick="confirmDelete(' . $row['id'] . ')">Hapus</button>';
+                                                        echo '</center>';
+                                                    } ?></td> -->
+
+                                    </tr>
+                                <?php
+                                }
+                                ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
     <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
     <script src="https://cdn.datatables.net/2.0.3/js/dataTables.min.js"></script>
     <script src="../../assets/datepicker/js/bootstrap-datepicker.js"></script>
+    <script>
+        $(function() {
+            $(".datepicker").datepicker({
+                format: 'dd/mm/yyyy',
+                autoclose: true,
+                todayHighlight: true,
+            });
+            // Initialize DataTable
+            table = new DataTable('#table_id', {
+                columnDefs: [{
+                    targets: [0],
+                    orderable: false,
+                    searchable: false
+                }, ]
+            });
+        });
+
+        ////cara panjang
+        // function change() {
+        //     var lokasi = $('#lokasi').val();
+        //     var kode_barang = $('#kode_barang').val();
+        //     var tgl_transaksi = $('#tgl_transaksi').val();
+        //     var bukti = $('#bukti').val();
+
+        //     if (kode_barang && lokasi && tgl_transaksi && bukti) {
+        //         window.location.href = 'index.php?kode_barang=' + kode_barang + '&lokasi=' + lokasi + '&tgl_transaksi=' + tgl_transaksi + '&bukti=' + bukti;
+        //     } else if (kode_barang && lokasi && tgl_transaksi) {
+        //         window.location.href = 'index.php?kode_barang=' + kode_barang + '&lokasi=' + lokasi + '&tgl_transaksi=' + tgl_transaksi;
+        //     } else if (kode_barang && lokasi && bukti) {
+        //         window.location.href = 'index.php?kode_barang=' + kode_barang + '&lokasi=' + lokasi + '&bukti=' + bukti;
+        //     } else if (kode_barang && tgl_transaksi && bukti) {
+        //         window.location.href = 'index.php?kode_barang=' + kode_barang + '&tgl_transaksi=' + tgl_transaksi + '&bukti=' + bukti;
+        //     } else if (lokasi && tgl_transaksi && bukti) {
+        //         window.location.href = 'index.php?lokasi=' + lokasi + '&tgl_transaksi=' + tgl_transaksi + '&bukti=' + bukti;
+        //     } else if (kode_barang && lokasi) {
+        //         window.location.href = 'index.php?kode_barang=' + kode_barang + '&lokasi=' + lokasi;
+        //     } else if (kode_barang && tgl_transaksi) {
+        //         window.location.href = 'index.php?kode_barang=' + kode_barang + '&tgl_transaksi=' + tgl_transaksi;
+        //     } else if (kode_barang && bukti) {
+        //         window.location.href = 'index.php?kode_barang=' + kode_barang + '&bukti=' + bukti;
+        //     } else if (lokasi && tgl_transaksi) {
+        //         window.location.href = 'index.php?lokasi=' + lokasi + '&tgl_transaksi=' + tgl_transaksi;
+        //     } else if (lokasi && bukti) {
+        //         window.location.href = 'index.php?lokasi=' + lokasi + '&bukti=' + bukti;
+        //     } else if (tgl_transaksi && bukti) {
+        //         window.location.href = 'index.php?tgl_transaksi=' + tgl_transaksi + '&bukti=' + bukti;
+        //     } else if (kode_barang) {
+        //         window.location.href = 'index.php?kode_barang=' + kode_barang;
+        //     } else if (lokasi) {
+        //         window.location.href = 'index.php?lokasi=' + lokasi;
+        //     } else if (tgl_transaksi) {
+        //         window.location.href = 'index.php?tgl_transaksi=' + tgl_transaksi;
+        //     } else if (bukti) {
+        //         window.location.href = 'index.php?bukti=' + bukti;
+        //     } else {
+        //         window.location.href = 'index.php';
+        //     }
+        // }
+
+        //cara sederhana
+        function change() {
+            var lokasi = $('#lokasi').val();
+            var kode_barang = $('#kode_barang').val();
+            var tgl_transaksi = $('#tgl_transaksi').val();
+            var bukti = $('#bukti').val();
+
+            var url = 'index.php';
+            if (kode_barang) url += '?kode_barang=' + kode_barang;
+            if (lokasi) url += (url.includes('?') ? '&' : '?') + 'lokasi=' + lokasi;
+            if (tgl_transaksi) url += (url.includes('?') ? '&' : '?') + 'tgl_transaksi=' + tgl_transaksi;
+            if (bukti) url += (url.includes('?') ? '&' : '?') + 'bukti=' + bukti;
+
+            window.location.href = url;
+        }
+    </script>
 </body>
 
 </html>
